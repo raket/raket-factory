ed4ee2a0c849fa20b29593989002cf79842274cc
danne_j87 <danne_j87@hotmail.com>
2019-01-14 14:40:02 +0100

    Change Grid

--- packages/grid/src/Grid.js
+++ packages/grid/src/Grid.js
@@ -1,48 +1,25 @@
 import React from 'react'
-import t from 'prop-types'
+import './style.scss'
 
-const Grid = (props) => {
-
-  const defaultStyle = {
-    display: 'grid',
-    gridGap: props.spacing,
-    gridTemplateColumns: `repeat(${props.columns},1fr)`,
-  }
-
-  const gridItemStyle = {
-    background: '#efefef',
-    border: '1px solid',
-    height: '200px'
-  }
-
+const Grid = ({columns = 3, children}) => {
+  const columnsClass = 'grid--'+columns
   return (
-    <div style={defaultStyle}>
+    <div className={'grid ' + columnsClass}>
       <div style={gridItemStyle}> </div>
       <div style={gridItemStyle}> </div>
       <div style={gridItemStyle}> </div>
       <div style={gridItemStyle}> </div>
       <div style={gridItemStyle}> </div>
       <div style={gridItemStyle}> </div>
-      {props.children}
+      {children}
     </div>
   )
 }
-
 export default Grid
 
 
-
-Grid.propTypes = {
-  /** Spacing between columns*/
-  spacing: t.string,
-  /** Nr of columns per row*/
-  columns: t.number,
-  /** Add classes to the grid */
-  classes: t.string,
-}
-
-Grid.defaultProps = {
-  spacing: '2rem',
-  classes: 'grid',
-  columns: 3
+const gridItemStyle = {
+  background: '#efefef',
+  border: '1px solid',
+  height: '200px'
 }
